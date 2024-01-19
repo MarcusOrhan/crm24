46c15993709326e1f4831c1042b8451be7a9c55f
glnsb <marcusorhan@gmail.com>
2024-01-19 00:05:18 -0500

    crm24 Project frame successfully created by team

---
+++ src/test/java/step_definitions/FirstTest.java
@@ -0,0 +1,13 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class FirstTest {
+    @Given("user land on website")
+    public void user_land_on_website() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+
+}
---
+++ src/test/resources/features/crm_login.feature
@@ -0,0 +1,4 @@
+@wip
+Feature: first test
+  Scenario: firstUydurukIsim
+    Given  user land on website
